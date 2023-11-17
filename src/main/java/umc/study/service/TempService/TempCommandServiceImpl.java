c041dcad26c4f00b9b7a3710261de20e48e443d8
kchaeeun <oceanball38@naver.com>
2023-11-18 01:31:12 +0900

    [FEAT] API 응답 통일 & 에러 핸들러

---
+++ src/main/java/umc/study/service/TempService/TempCommandServiceImpl.java
@@ -0,0 +1,11 @@
+package umc.study.service.TempService;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import umc.study.apiPayload.exception.handler.TempHandler;
+
+@Service
+@RequiredArgsConstructor
+public class TempCommandServiceImpl implements TempCommandService{
+
+}
---
+++ src/main/java/umc/study/web/dto/TempResponse.java
@@ -0,0 +1,25 @@
+package umc.study.web.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+public class TempResponse {
+
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class TempTestDTO{    // 범용적으로 사용하기 위함
+        String testString;
+    }
+
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class TempExceptionDTO{
+        Integer flag;
+    }
+}
